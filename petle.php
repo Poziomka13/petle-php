21a0d70b2f6941a0f3f1b759a95952a419f4ee12
Poziomka13 <156078717+Poziomka13@users.noreply.github.com>
2024-01-15 09:09:49 +0100

    Add files via upload

---
+++ petle.php
@@ -0,0 +1,51 @@
+<?php 
+
+
+//zadanie1
+// for($i = 1; $i <= 20; $i++)
+// {
+//  echo 'c';
+//  if($i % 10 == 0)
+//  {
+//     echo "\n";
+//     echo '<br>';
+//  }
+// }
+
+//zadanie2
+// for($i = 1; $i <= 100; $i++)
+// {
+//     echo $i .'';
+// }
+// echo '<br>';
+
+// $i = 1;
+// while ($i <= 100)
+// {
+//     echo $i .' ';
+//     $i++;
+// }
+
+// $i = 97;
+// do
+// {
+//     echo $i . " ";
+//     $i -= 4;
+// }
+// while($i >= 1);
+
+//zadanie 3 
+
+// $n = 10;
+// while($n >0)
+// {
+// echo "kwadrat $n:"  .$n** 2 . "\n";
+// if ($n % 2 == 0)
+// {
+//     echo "kwadrat $n dla parzystej:" . ($n ** 2) . "\n";
+// }
+
+// $n--;
+// }
+
+// ?>
